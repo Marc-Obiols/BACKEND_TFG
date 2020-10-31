95e62bc3a5173f8bb72d4f88d8928360d3b5bb0d
98obi <98obiols@gmail.com>
2020-10-31 21:03:12 +0100

    Filtrar ejercicios0

--- src/routes/ejercicio.js
+++ src/routes/ejercicio.js
@@ -43,14 +43,71 @@ router.get('/image/:nombre', async (req, res) => {
     }
 });
 
-router.get('/ejercicio/:nombre', async (req, res) => {
+//Get ejercicios filtrados
+router.post('/filtrados', async (req, res) => {
+
+
+    var dificultad = req.body.dificultad
+    var musculos = req.body.musculos
+    var nombre = req.body.nombre
+    console.log("[" + dificultad + " " + musculos + " " + nombre + "]")
+    var estiramiento = req.body.estiramiento //1 estiramientos 0 aerobicos 2 todos
+
+    try {
+        const ejercicios = await Ejercicio.find({});
+        let nombres_ejercicios = [];
+        for(let i = 0; i < ejercicios.length; i++) {
+            var aux = 0
+            for(let j = 0; j < ejercicios[i].musculos.length; j++) {
+                if(musculos.includes(ejercicios[i].musculos[j])) aux = aux + 1
+            }
+
+            console.log("Musculos: " + (aux == musculos.length  || musculos[0].localeCompare("") == 0))
+            console.log("Estiramiento: " + (ejercicios[i].estiramiento == estiramiento || estiramiento == 2))
+            console.log("Nombre: " + (ejercicios[i].nombre.localeCompare(nombre) == 0 || nombre.localeCompare("") == 0))
+            console.log("Dificultad: " + (ejercicios[i].dificultad.localeCompare(dificultad) == 0 || dificultad.localeCompare("") == 0))
+
+            if((aux == musculos.length  || musculos[0].localeCompare("") == 0) && 
+            (ejercicios[i].nombre.localeCompare(nombre) == 0 || nombre.localeCompare("") == 0) && 
+            (ejercicios[i].dificultad.localeCompare(dificultad) == 0 || dificultad.localeCompare("") == 0) && 
+            (ejercicios[i].estiramiento == estiramiento || estiramiento == 2)) {
+                nombres_ejercicios.push(ejercicios[i].nombre)
+            }
+        }
+        console.log(nombres_ejercicios)
+        res.status(200).json(nombres_ejercicios);
+    } catch(err) {
+        console.log("error: " + err)
+        res.status(413).json(err);
+    }
+});
+
+//Get info de un ejercicio
+router.get('/:nombre', async (req, res) => {
     var nombre = req.params.nombre;
     try {
         const ejercicio = await Ejercicio.findOne({nombre: nombre });
         if (isEmpty(ejercicio)) {
             res.status(400).json("Internal DB error");
         }
+        ejercicio.imagen = null
         return res.status(200).send(ejercicio);
+    } catch(err) {
+        console.log("error: " + err)
+        res.status(413).json(err);
+    }
+});
+
+//Get nombres de todos los ejercicios
+router.get('/', async (req, res) => {
+
+    try {
+        const ejercicios = await Ejercicio.find({});
+        let nombres_ejercicios = [];
+        for(let i = 0; i < ejercicios.length; i++) {
+            nombres_ejercicios.push(ejercicios[i].nombre)
+        }
+        res.status(200).json(nombres_ejercicios);
     } catch(err) {
         console.log("error: " + err)
         res.status(413).json(err);
